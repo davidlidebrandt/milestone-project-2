18ba4803be66bfbc6c6e69a7f88cb3f33e51ae56
davidlidebrandt <david.lidebrandt@gmail.com>
2020-12-04 15:02:12 +0000

    created custom toggle button for sound setting

--- assets/javascript/myjs.js
+++ assets/javascript/myjs.js
@@ -12,5 +12,21 @@ $( document ).ready(function() {
     $(this).parent().hide(500);
   });
 
+  $("#sound-btn").click(function() {
+     if($(this).hasClass("off-btn")) {
+         $(this).addClass("on-btn");
+         $(this).removeClass("off-btn");
+         $(".circle-on").addClass("circle-off");
+         $(".circle-off").removeClass("circle-on");
+     }
+
+      else if($(this).hasClass("on-btn")) {
+         $(this).addClass("off-btn");
+         $(this).removeClass("on-btn");
+         $(".circle-off").addClass("circle-on");
+         $(".circle-on").removeClass("circle-off");
+     }
+  });
+
 
 }); //end of ready function

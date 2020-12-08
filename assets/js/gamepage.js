227852448644838a083ea6a0dd966f2574e4a6f9
davidlidebrandt <david.lidebrandt@gmail.com>
2020-12-08 14:56:22 +0000

    added function that countsdown the timer and changes its color in intervals

--- assets/js/gamepage.js
+++ assets/js/gamepage.js
@@ -1,3 +1,7 @@
+ localStorage.setItem("time", "20")
+
+ let timerreturn;
+ 
  $(document).ready(function() {
  
  if(sessionStorage.getItem("sound")=="on") {
@@ -5,5 +9,29 @@
       $("#sound audio source").attr("src", "assets/sounds/ticking.mp3");
 
   }
+//placeholder method for restarting the timer
+  $("#timer").click(function(){
+     clearInterval(timerreturn);
+     countDownTimer();
+  });
 
 }); //end of ready function
+
+
+function countDownTimer() {
+  
+  $("#timer").removeClass("yellow-timer");
+  $("#timer").removeClass("red-timer");
+
+  time = localStorage.getItem("time");
+  
+  timerreturn = setInterval(function(){$("#timer").html(time + " SECONDS LEFT"); time--;  
+  if(time < 12) {$("#timer").addClass("yellow-timer"); }
+  if(time < 6) {$("#timer").addClass("red-timer"); }
+  if(time < 0){$("#timer").hide(); } 
+  }, 1000);
+ 
+};
+
+countDownTimer();
+

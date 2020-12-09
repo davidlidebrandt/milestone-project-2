0a6046cf37c2428ff0406b62f21a4e95012315a1
davidlidebrandt <david.lidebrandt@gmail.com>
2020-12-09 13:59:23 +0000

    added function that genarates random card class

--- assets/js/gamepage.js
+++ assets/js/gamepage.js
@@ -28,10 +28,30 @@ function countDownTimer() {
   timerreturn = setInterval(function(){$("#timer").html(time + " SECONDS LEFT"); time--;  
   if(time < 12) {$("#timer").addClass("yellow-timer"); }
   if(time < 6) {$("#timer").addClass("red-timer"); }
-  if(time < 0){$("#timer").hide(); } 
+  if(time < 0) {$("#timer").hide(); }
   }, 1000);
  
 };
+
+function generateRandomClass () {
+  
+  let cardclasses = ["red","red-2","blue","blue-2","green","green-2","yellow", "yellow-2"
+  ,"red","red-2","blue","blue-2","green","green-2","yellow", "yellow-2"];
+  let idchooser = 1;
+  let copyofcardclasses = cardclasses.slice();
+  let numberofcards = 16;
+  let initalclasses = 16;
+
+  for(let i = 0; i < initalclasses; i++) {
+    let currentclass = copyofcardclasses[Math.floor(Math.random() * (numberofcards))];
+    $("#" + idchooser).addClass(currentclass);  
+    copyofcardclasses.splice(copyofcardclasses.indexOf(currentclass),1);
+    console.log(currentclass);
+    idchooser++;
+    numberofcards --;
+  }
+}
+generateRandomClass();
 
 countDownTimer();
 
